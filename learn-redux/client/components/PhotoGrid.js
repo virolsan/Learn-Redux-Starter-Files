7f74f801f292e534c470e1fa386c9afcd1fc60c0
Santtu Virolainen <santtu.virolainen@solita.fi>
2018-07-24 11:42:50 +0300

    Steps 1-10

---
+++ learn-redux/client/components/PhotoGrid.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PhotoGrid = () => (
+    <div className="photo-grid">
+        I'm the grid
+    </div>
+);
+
+export default PhotoGrid;
---
+++ learn-redux/client/components/Single.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Single = () => (
+    <div className="single-photo">
+        I'm the single
+    </div>
+);
+
+export default Single;
